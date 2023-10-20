4aaaa0b2d401bcf4d474083294d7154dea27e90e
sebastianmarines <a01383056@tec.mx>
2023-10-20 00:37:25 -0600

    Create privacy policy page

--- app/src/main/java/com/example/navdrawer/screens/register/RegisterPage.kt
+++ app/src/main/java/com/example/navdrawer/screens/register/RegisterPage.kt
@@ -1,18 +1,21 @@
 package com.example.navdrawer.screens.register
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -25,8 +28,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.navdrawer.AppViewModel
@@ -40,6 +46,7 @@ fun RegisterPage(navController: NavController, viewModel: AppViewModel) {
     var numeroTelefonico by remember { mutableStateOf("") }
     var estado by remember { mutableStateOf("") }
     var ciudad by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf(false) }
 
     Surface(color = MaterialTheme.colorScheme.background, modifier = Modifier.fillMaxSize()) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
@@ -48,50 +55,112 @@ fun RegisterPage(navController: NavController, viewModel: AppViewModel) {
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier
                     .padding(32.dp)
-                    .background(MaterialTheme.colorScheme.surface, shape = RoundedCornerShape(16.dp))
+                    .background(
+                        MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(16.dp)
+                    )
                     .padding(24.dp)
             ) {
                 Text("Registro de usuario", style = MaterialTheme.typography.headlineLarge)
 
-                Text(text = viewModel.getLoginError(), style = MaterialTheme.typography.bodyMedium, color = MaterialTheme.colorScheme.error)
+                Text(
+                    text = viewModel.getLoginError(),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.error
+                )
 
                 OutlinedTextField(
                     value = nombre,
                     onValueChange = { nombre = it },
                     label = { Text("Nombre") },
                     leadingIcon = { Icon(Icons.Default.Person, contentDescription = null) },
-                    modifier = Modifier.fillMaxWidth().padding(8.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
                 )
                 OutlinedTextField(
                     value = apellido,
                     onValueChange = { apellido = it },
                     label = { Text("Apellido") },
                     leadingIcon = { Icon(Icons.Default.Person, contentDescription = null) },
-                    modifier = Modifier.fillMaxWidth().padding(8.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
                 )
                 OutlinedTextField(
                     value = correo,
                     onValueChange = { correo = it },
                     label = { Text("Correo") },
                     leadingIcon = { Icon(Icons.Default.Email, contentDescription = null) },
-                    modifier = Modifier.fillMaxWidth().padding(8.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
                 )
                 OutlinedTextField(
                     value = contrasena,
                     onValueChange = { contrasena = it },
                     label = { Text("Contraseña") },
                     visualTransformation = PasswordVisualTransformation(),
-                    modifier = Modifier.fillMaxWidth().padding(8.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
                 )
                 OutlinedTextField(
                     value = numeroTelefonico,
                     onValueChange = { numeroTelefonico = it },
                     label = { Text("Número telefónico") },
-                    modifier = Modifier.fillMaxWidth().padding(8.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
                 )
+                // Confirmacion de aviso de privacidad
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(2.dp)
+                ) {
+                    val annotatedString = buildAnnotatedString {
+                        append("Acepto el ")
+                        pushStringAnnotation(
+                            tag = "URL",
+                            annotation = "https://www.google.com"
+                        )
+                        withStyle(
+                            SpanStyle(color = Color.Blue)
+                        ) {
+                            append("aviso de privacidad")
+                        }
+                        pop()
+                    }
+
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = { checked = it },
+                    )
+
+                    ClickableText(
+                        text = annotatedString,
+                        onClick = { offset ->
+                            annotatedString.getStringAnnotations(
+                                tag = "URL",
+                                start = offset,
+                                end = offset
+                            ).firstOrNull()?.let { annotation ->
+                                Log.d("RegisterPage", "Clicked on: ${annotation.item}")
+                                navController.navigate("SecurityPage")
+                            }
+                        },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
 
                 Button(
                     onClick = {
+                        if (!checked) {
+                            viewModel.setLoginError("Debes aceptar el aviso de privacidad")
+                            return@Button
+                        }
                         viewModel.registerUser(
                             first_name = nombre,
                             last_name = apellido,
@@ -102,7 +171,9 @@ fun RegisterPage(navController: NavController, viewModel: AppViewModel) {
                             nav = navController
                         )
                     },
-                    modifier = Modifier.fillMaxWidth().padding(8.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
                 ) {
                     Text("Registrarme")
                 }
--- app/src/main/java/com/example/navdrawer/screens/seguridad/SecurityPage.kt
+++ app/src/main/java/com/example/navdrawer/screens/seguridad/SecurityPage.kt
@@ -7,6 +7,6 @@ import androidx.compose.runtime.Composable
 @Composable
 fun SecurityPage() {
     Column {
-        Text(text = "Aviso de privacidad")
+        Text(text = "Tu privacidad es importante para nosotros. Durante el demo, recopilamos información personal mínima, como tu nombre y correo electrónico, exclusivamente para brindarte información sobre nuestros productos y servicios. No compartiremos tus datos. Puedes solicitar acceso o corrección de tu información.")
     }
 }
